61922299746b979ecc876040d7b9e5e9231b15e6
Richard Silveira <richardleecba@gmail.com>
2020-05-02 16:56:43 -0400

    initial setup for JWT authentication

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import userRoutes from './src/routes/userRoutes'
 import mongoose, {Schema} from 'mongoose'
 import bodyParser from 'body-parser'
+import jsonwebtoken from 'jsonwebtoken';
+import dotenv from 'dotenv-safe'
+
+dotenv.config()
 
 const app = express()
 const PORT = 4000
@@ -35,6 +39,23 @@ mongoose.connect('mongodb://root:example@localhost/users?authSource=admin', {
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+// JWT Header setup
+app.use((req, res, next) => {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+        jsonwebtoken.verify(req.headers.authorization.split(' ')[1], process.env.SECRET, (err, decode) => {
+            if (err) req.user = undefined;
+            req.user = decode;
+            console.log(`Authenticated user ${req.user}`)
+
+            next();
+        });
+    } else {
+        req.user = undefined;
+        console.log('Anonymous user')
+        next();
+    }
+});
 
 userRoutes(app)//todo: it sounds like not a good practice, research about it (probably I should be a centralized route module
 
